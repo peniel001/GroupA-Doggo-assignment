d03ee43aca86626532d671bf24b14150a1523419
peniel001 <penielcrypt@gmail.com>
2026-06-12 19:01:30 +0100

    eidthed the commit message below. Lines starting with '#' will be ignored, and an empty message aborts the commit.

--- script.js
+++ script.js
@@ -17,8 +17,6 @@ function shuffleArray(array) {
 
 
 // TODO 1
-// Given an array of possible answers, a correct answer value, and a number of choices to get,
-// return a list of that many choices, including the correct answer and others from the array
 
 function getMultipleChoices(n, correctAnswer, array) {
     let choices = []; // Start with the correct answer in the choices array
